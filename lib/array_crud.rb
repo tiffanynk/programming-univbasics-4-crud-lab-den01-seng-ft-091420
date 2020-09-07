54c8ba3ae5563ecde562ee5779a28fac1574fff6
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-07 19:38:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  climbing_gear = ["shoes", "chalk", "quickdraws", "grigri"]
+  ["shoes", "chalk", "quickdraws", "grigri"]
 end
 
 def add_element_to_end_of_array(array, element)
